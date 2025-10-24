0954d6cac744654bae3b3d2c2addc40ab8137168
neo-the-coder <kamranz150@gmail.com>
2025-10-24 06:47:45 -0400

    add: Login endpoint

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,12 +1,54 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './LoginPage.css';
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+            navigate('/app');
+        }
+    }, [navigate])
 
     const handleLogin = async () => {
-        console.log('Logg')
+        try{
+            const response = await fetch(`/api/auth/login`, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                    'Authorization': bearerToken ? `Bearer ${bearerToken}` : '',
+                },
+                body: JSON.stringify({
+                    email,
+                    password
+                })
+            })
+
+            const json = await response.json();
+
+            if (json.authtoken) {
+                sessionStorage.setItem('auth-token', json.authtoken);
+                sessionStorage.setItem('name', json.userName);
+                sessionStorage.setItem('email', json.userEmail);
+    
+                setIsLoggedIn(true);
+                navigate('/app')
+            } else {
+                document.getElementById("email").value = "";
+                document.getElementById("password").value = "";
+                setError("Wrong password. Try again.");
+            }
+          }catch (e) {
+            console.log("Error fetching details: " + e.message);
+        }
     }
 
     return (
@@ -24,6 +66,8 @@ function LoginPage() {
 
                     <button className="btn btn-primary w-100 mb-3" type="submit">Login</button>
                 </form>
+
+                <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{error}</span>
                 <p className="mt-4 text-center">
                     New here? <a href="/app/register" className="text-primary">Register Here</a>
                 </p>
